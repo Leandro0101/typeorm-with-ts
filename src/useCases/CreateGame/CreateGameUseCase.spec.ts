3681822f5aef5bd9f30209182fcac2fed07b9a16
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-15 21:14:49 -0300

    test: Should return 201 when a game is created

--- src/useCases/CreateGame/CreateGameUseCase.spec.ts
+++ src/useCases/CreateGame/CreateGameUseCase.spec.ts
@@ -39,15 +39,15 @@ describe('CreateGameUseCase', () => {
     expect(response.status).toBe(400)
   })
 
-  // test('Should return 201 when a game is created', async () => {
-  //   const response = await request(app).post('/games').send({
-  //     name: 'any name',
-  //     description: 'any description',
-  //     price: 20
-  //   })
+  test('Should return 201 when a game is created', async () => {
+    const response = await request(app).post('/games').send({
+      name: 'any name',
+      description: 'any description',
+      price: 20
+    })
 
-  //   expect(response.status).toBe(201)
-  // })
+    expect(response.status).toBe(201)
+  })
 
   // test('Should return the created game DTO', async () => {
   //   const response = await request(app).post('/games').send({
