65afa3931d0462a7f37750ded515ba9fc4d09ff0
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-27 19:17:56 -0300

    validate: fields is empty in create and update game

--- src/useCases/game/Create/CreateUseCase.ts
+++ src/useCases/game/Create/CreateUseCase.ts
@@ -14,7 +14,7 @@ export class CreateUseCase {
     const requiredFields = ['name', 'description', 'price']
 
     for (const field of requiredFields) {
-      if (!data[field]) {
+      if (!data[field] || !/[a-z]|[0-9]/ig.test(data[field])) {
         throw new Exception().handler(new MissingParamError(), 400)
       }
     }
